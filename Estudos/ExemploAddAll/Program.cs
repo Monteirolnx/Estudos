df6acb6acb6830c0a5889e115a750d5fdd3d184f
Monteiro <lfmleitao@SF.PREFEITURA.SP.GOV.BR>
2022-11-10 09:24:33 -0300

    [Monteiro] Exemplo INumber.

---
+++ Estudos/ExemploAddAll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+var resultInt = AddAll(new[] { 1, 2, 3, 4, 5 });
+Console.WriteLine(resultInt);
+
+var resultDouble = AddAll(new[] { 1, .2, 3, 4, 5 });
+Console.WriteLine(resultDouble);
+
+Console.ReadKey();
+
+static T AddAll<T>(IEnumerable<T> values) where T: INumber<T>
+{
+    return values.Aggregate(T.Zero, (current, value) => current + value);
+}
+
+//T AddAll<T>(T[] values) where T : INumber<T>
+//{
+//    var result = T.Zero;
+//    foreach (var value in values)
+//    {
+//        result += value;
+//    }
+
+//    return result;
+//}
