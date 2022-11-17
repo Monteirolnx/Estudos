7502d07edad7fe9a9cc88f9396d6f5ce979b3c3b
Monteiro <lfmleitao@SF.PREFEITURA.SP.GOV.BR>
2022-11-17 13:23:59 -0300

    [Monteiro] Exemplo propriedades com required.

---
+++ Estudos/ExamploRequired/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var custumer = new Custumer()
+{
+    FirstName = "Luis",
+    LastName = "Monteiro",
+    Age = 40
+};
+
+Console.WriteLine(custumer.ToString());
+
+Console.ReadLine();
+
+public class Custumer
+{
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"FirstName: {FirstName}, LastName: {LastName}, Age: {Age}";
+    }
+}   
